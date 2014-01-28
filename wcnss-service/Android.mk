9063d894995f12a0c213eeb05c449d5f5fb384c6
dhacker29 <dhackerdvm@gmail.com>
2014-01-28 08:44:01 -0600

    wcnss-service: Preseve old behavior as default
    Set TARGET_USES_WCNSS_CTRL := true in Boardconfig to use
    the new /dev/wcnss_ctrl and ini in system/etc/wifi
    
    Change-Id: I9912ab03e71c8e0ed4dfd4296e5f12b465ae29d5

--- wcnss-service/Android.mk
+++ wcnss-service/Android.mk
@@ -3,7 +3,11 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := wcnss_service
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/common/inc/
+ifeq ($(TARGET_USES_WCNSS_CTRL),true)
 LOCAL_SRC_FILES := wcnss_service.c
+else
+LOCAL_SRC_FILES := wcnss_service_old.c
+endif
 LOCAL_SHARED_LIBRARIES := libc libcutils libutils
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -Wall
