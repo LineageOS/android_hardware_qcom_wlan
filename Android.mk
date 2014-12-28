0459310cb444f26aeb5268b3d5feebbe495c12a9
Steve Kondik <steve@cyngn.com>
2014-12-28 02:32:53 -0800

    wlan: Add guard makefile
    Change-Id: I4f53cf6a5cfd204f13ca13b3dcdb6dc83529837a

--- Android.mk
+++ Android.mk
@@ -1,4 +1,8 @@
+ifeq ($(call my-dir),$(call project-path-for,wlan))
+
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(BOARD_IS_AUTOMOTIVE),true)
 include $(call all-subdir-makefiles)
+endif
+
 endif
