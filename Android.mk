4a0315bfbacf5c1faa915cad0ef8550b4d314396
Steve Kondik <steve@cyngn.com>
2014-12-28 02:32:53 -0800

    wlan: Add guard makefile
    Change-Id: I4f53cf6a5cfd204f13ca13b3dcdb6dc83529837a

--- Android.mk
+++ Android.mk
@@ -1,5 +1,10 @@
 # TODO:  Fix this properly when b/37901207 is fixed
 #ifneq ($(BOARD_IS_AUTOMOTIVE),true)
+
+WLAN_PATH := $(call my-dir)
+
+ifeq ($(WLAN_PATH),$(call project-path-for,wlan))
 ifeq ($(filter bat,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles)
+endif
 endif
