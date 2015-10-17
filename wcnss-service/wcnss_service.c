95f8d2f20b8593f2031dacb1e84923234c0999cc
Steve Kondik <steve@cyngn.com>
2015-10-16 21:15:20 -0700

    wcnss_service: Deal with mdm-detect too
    Change-Id: I1f6ad7502bb23fd59f182c1fe86ba92cb47470b7

--- wcnss-service/wcnss_service.c
+++ wcnss-service/wcnss_service.c
@@ -43,7 +43,9 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <cutils/properties.h>
 #ifdef WCNSS_QMI
 #include "wcnss_qmi_client.h"
+#ifdef MDM_DETECT
 #include "mdm_detect.h"
+#endif
 #endif
 #ifdef WCNSS_QMI_OSS
 #include <dlfcn.h>
@@ -515,7 +517,7 @@ void setup_wlan_driver_ath_prop()
 	property_set("vendor.wlan.driver.ath", WLAN_DRIVER_ATH_DEFAULT_VAL);
 }
 
-#ifdef WCNSS_QMI
+#ifdef MDM_DETECT
 int check_modem_compatability(struct dev_info *mdm_detect_info)
 {
 	char args[MODEM_BASEBAND_PROPERTY_SIZE] = {0};
@@ -798,8 +800,10 @@ int main(int argc, char *argv[])
 	int fd_dev, ret_cal;
 #if defined(WCNSS_QMI) || defined(WCNSS_QMI_OSS)
 	int nv_mac_addr = FAILED;
+#ifdef MDM_DETECT
 	struct dev_info mdm_detect_info;
 	int nom = 0;
+#endif
 #endif
 
 	setup_wlan_config_file();
@@ -827,6 +831,7 @@ int main(int argc, char *argv[])
 	}
 #endif
 #ifdef WCNSS_QMI
+#ifdef MDM_DETECT
 	/* Call ESOC API to get the number of modems.
 	   If the number of modems is not zero, only then proceed
 	   with the eap_proxy intialization.*/
@@ -848,6 +853,7 @@ int main(int argc, char *argv[])
 		ALOGE("wcnss_service: Target does not have external modem");
 		goto nomodem;
 	}
+#endif
 
 	/* initialize the DMS client and request the wlan mac address */
 
