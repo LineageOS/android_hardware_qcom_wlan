b7a36a100fc3296754bb3ec3f6acf96def241bf1
Danny Baumann <dannybaumann@web.de>
2016-09-05 13:18:31 +0200

    Fix build for devices that set TARGET_PROVIDES_WCNSS_QMI.
    Change-Id: Idd1a57a8343199df838f3a25bbb49b2f21dfab4d

--- wcnss-service/wcnss_service.c
+++ wcnss-service/wcnss_service.c
@@ -371,7 +371,7 @@ unsigned int convert_string_to_hex(char* string)
 }
 
 
-#ifdef WCNSS_QMI
+#if defined(WCNSS_QMI) || defined(WCNSS_QMI_OSS)
 void setup_wcnss_parameters(int *cal, int nv_mac_addr)
 #else
 void setup_wcnss_parameters(int *cal)
@@ -578,8 +578,10 @@ int main(int argc, char *argv[])
 	UNUSED(argc), UNUSED(argv);
 	int rc;
 	int fd_dev, ret_cal;
-#ifdef WCNSS_QMI
+#if defined(WCNSS_QMI) || defined(WCNSS_QMI_OSS)
 	int nv_mac_addr = FAILED;
+#endif
+#ifdef WCNSS_QMI
 #ifdef MDM_DETECT
 	struct dev_info mdm_detect_info;
 #endif
@@ -655,7 +657,7 @@ int main(int argc, char *argv[])
 nomodem:
 #endif
 
-#ifdef WCNSS_QMI
+#if defined(WCNSS_QMI) || defined(WCNSS_QMI_OSS)
 	setup_wcnss_parameters(&ret_cal, nv_mac_addr);
 #else
 	setup_wcnss_parameters(&ret_cal);
