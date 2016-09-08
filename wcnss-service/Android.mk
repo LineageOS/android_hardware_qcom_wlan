b6b2caef1d920328902481cca17f7723e1813cdc
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-09-08 10:09:50 -0700

    wcnss-service: Enable and fix -Werror build
    Change-Id: Ie4de8c5b05f948ade26157de8a13c1dbbef0f383

--- wcnss-service/Android.mk
+++ wcnss-service/Android.mk
@@ -33,7 +33,7 @@ endif #TARGET_PROVIDES_WCNSS_QMI
 endif #TARGET_USES_QCOM_WCNSS_QMI
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -Wall
+LOCAL_CFLAGS += -Wall -Werror
 
 include $(BUILD_EXECUTABLE)
 
@@ -61,7 +61,7 @@ LOCAL_SRC_FILES += wcnss_qmi_client.c
 
 LOCAL_MODULE := libwcnss_qmi
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -Wall
+LOCAL_CFLAGS += -Wall -Werror
 
 include $(BUILD_SHARED_LIBRARY)
 
--- wcnss-service/wcnss_service.c
+++ wcnss-service/wcnss_service.c
@@ -793,8 +793,8 @@ int main(int argc, char *argv[])
 	int nv_mac_addr = FAILED;
 #ifdef MDM_DETECT
 	struct dev_info mdm_detect_info;
-#endif
 	int nom = 0;
+#endif
 #endif
 
 	setup_wlan_config_file();
