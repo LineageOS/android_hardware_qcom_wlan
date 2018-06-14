affb8bde8d8251d653867686fe5b58541f198312
Srinivas Dasari <dasaris@codeaurora.org>
2018-06-14 16:55:55 +0530

    WiFi-Hal: Sync qca-vendor_copy.h to upstream qca-vendor.h
    Sync qca-vendor_copy.h to upstream qca-vendor.h and remove
    duplicate enums/defines from vendor_definitions.h
    
    Bug: 121155020
    Test: Manual
    CRs-Fixed: 2290651
    
    Change-Id: I44e1c955ff2e4b207063ce5d7b88bb2bb8fb4ed6

--- qcwcn/wifi_hal/wifi_hal.cpp
+++ qcwcn/wifi_hal/wifi_hal.cpp
@@ -52,7 +52,6 @@
 #include "cpp_bindings.h"
 #include "ifaceeventhandler.h"
 #include "wifiloggercmd.h"
-#include "vendor_definitions.h"
 
 /*
  BUGBUG: normally, libnl allocates ports for all connections it makes; but
@@ -1342,7 +1341,7 @@ wifi_error wifi_start_sending_offloaded_packet(wifi_request_id id,
         goto cleanup;
 
     ret = vCommand->put_bytes(
-            QCA_WLAN_VENDOR_ATTR_OFFLOADED_PACKETS_IP_PACKET,
+            QCA_WLAN_VENDOR_ATTR_OFFLOADED_PACKETS_IP_PACKET_DATA,
             (const char *)ip_packet, ip_packet_len);
     if (ret != WIFI_SUCCESS)
         goto cleanup;
@@ -1419,6 +1418,8 @@ cleanup:
     return ret;
 }
 
+#define PACKET_FILTER_ID 0
+
 static wifi_error wifi_set_packet_filter(wifi_interface_handle iface,
                                          const u8 *program, u32 len)
 {
@@ -1458,7 +1459,7 @@ static wifi_error wifi_set_packet_filter(wifi_interface_handle iface,
                                 PACKET_FILTER_ID);
         if (ret != WIFI_SUCCESS)
             goto cleanup;
-        ret = vCommand->put_u32(QCA_WLAN_VENDOR_ATTR_PACKET_FILTER_TOTAL_LENGTH,
+        ret = vCommand->put_u32(QCA_WLAN_VENDOR_ATTR_PACKET_FILTER_SIZE,
                                 len);
         if (ret != WIFI_SUCCESS)
             goto cleanup;
@@ -1541,7 +1542,7 @@ static wifi_error wifi_get_packet_filter_capabilities(
         goto cleanup;
 
     ret = vCommand->put_u32(QCA_WLAN_VENDOR_ATTR_PACKET_FILTER_SUB_CMD,
-                            QCA_WLAN_GET_PACKET_FILTER_SIZE);
+                            QCA_WLAN_GET_PACKET_FILTER);
     if (ret != WIFI_SUCCESS)
         goto cleanup;
 
@@ -1669,7 +1670,7 @@ wifi_error wifi_write_packet_filter(wifi_interface_handle iface,
                                 PACKET_FILTER_ID);
         if (ret != WIFI_SUCCESS)
             goto cleanup;
-        ret = vCommand->put_u32(QCA_WLAN_VENDOR_ATTR_PACKET_FILTER_TOTAL_LENGTH,
+        ret = vCommand->put_u32(QCA_WLAN_VENDOR_ATTR_PACKET_FILTER_SIZE,
                                 len);
         if (ret != WIFI_SUCCESS)
             goto cleanup;
@@ -1844,7 +1845,7 @@ static wifi_error wifi_read_packet_filter(wifi_interface_handle handle,
 
         currentLength = min(remainingLengthToBeRead, info->firmware_bus_max_size);
 
-        ret = vCommand->put_u32(QCA_WLAN_VENDOR_ATTR_PACKET_FILTER_TOTAL_LENGTH,
+        ret = vCommand->put_u32(QCA_WLAN_VENDOR_ATTR_PACKET_FILTER_SIZE,
                                 currentLength);
         if (ret != WIFI_SUCCESS)
             break;
