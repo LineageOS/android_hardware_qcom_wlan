cba55e93eb13a1ba9dffcf946a1cf20e74d7840e
Bruno Martins <bgcngm@gmail.com>
2018-11-02 11:17:51 +0000

    wifi-hal: Use generated kernel headers
    Change-Id: I35832db9d5432a74a84420c3e5adbe522d960c23

--- qcwcn/wifi_hal/Android.mk
+++ qcwcn/wifi_hal/Android.mk
@@ -56,9 +56,6 @@ LOCAL_C_INCLUDES += \
 	external/libnl/include \
 	$(call include-path-for, libhardware_legacy)/hardware_legacy \
 	external/wpa_supplicant_8/src/drivers \
-	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
-
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_SRC_FILES := \
 	list.cpp \
@@ -100,6 +97,7 @@ LOCAL_C_INCLUDES += external/libnl-headers
 endif
 
 LOCAL_HEADER_LIBRARIES := libcutils_headers libutils_headers libwifi-hal-ctrl_headers
+LOCAL_HEADER_LIBRARIES += generated_kernel_headers
 LOCAL_SANITIZE := cfi signed-integer-overflow unsigned-integer-overflow
 
 include $(BUILD_STATIC_LIBRARY)
@@ -126,9 +124,6 @@ LOCAL_C_INCLUDES += \
 	external/libnl/include \
 	$(call include-path-for, libhardware_legacy)/hardware_legacy \
 	external/wpa_supplicant_8/src/drivers \
-	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
-
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_SRC_FILES := \
 	list.cpp \
@@ -173,5 +168,6 @@ LOCAL_C_INCLUDES += external/libnl-headers
 endif
 
 LOCAL_HEADER_LIBRARIES := libcutils_headers libutils_headers libwifi-hal-ctrl_headers
+LOCAL_HEADER_LIBRARIES += generated_kernel_headers
 LOCAL_SANITIZE := cfi integer_overflow
 include $(BUILD_SHARED_LIBRARY)
