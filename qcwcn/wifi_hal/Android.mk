39eb2935a0e95c4ca630056a8844dc18cbf9e945
Peng Xu <pxu@codeaurora.org>
2019-03-13 13:03:20 -0700

    wifi-hal: Add LOCAL_ADDITIONAL_DEPENDENCIES
    Wifi-hal module depends on the kernel headers being available,
    hence add this dependency.
    
    Change-Id: Ic77afa43ced8f12fc6a43f4388a2c5dac372c052
    CRs-fixed: 2415623

--- qcwcn/wifi_hal/Android.mk
+++ qcwcn/wifi_hal/Android.mk
@@ -40,6 +40,8 @@ LOCAL_C_INCLUDES += \
 	$(TARGET_OUT_HEADERS)/libwpa_client \
 	$(TARGET_OUT_HEADERS)/cld80211-lib
 
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
 LOCAL_SRC_FILES := \
 	wifi_hal.cpp \
 	common.cpp \
@@ -106,6 +108,8 @@ LOCAL_C_INCLUDES += \
 	external/wpa_supplicant_8/src/drivers \
 	$(TARGET_OUT_HEADERS)/libwpa_client \
 	$(TARGET_OUT_HEADERS)/cld80211-lib
+
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_SRC_FILES := \
 	wifi_hal.cpp \
