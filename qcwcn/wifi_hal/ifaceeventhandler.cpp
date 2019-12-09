9a4a4f53c50893c53486f740303275eb0838f7de
Tanmay Garg <tanmayg@codeaurora.org>
2019-12-09 15:06:07 +0530

    Revert "Update logging for type change of feature_set."
    This reverts commit cd1d91d93f36a80c4d489ac6c08cef2766e3f081.
    
    Change-Id: I489917eff3919afb3388eaee7695c56655708c2d
    CRs-Fixed: 2582497

--- qcwcn/wifi_hal/ifaceeventhandler.cpp
+++ qcwcn/wifi_hal/ifaceeventhandler.cpp
@@ -244,7 +244,7 @@ int WifihalGeneric::handleResponse(WifiEvent &reply)
                     return -EINVAL;
                 }
                 mSet = nla_get_u32(tb_vendor[QCA_WLAN_VENDOR_ATTR_FEATURE_SET]);
-                ALOGV("Supported feature set : %" PRIx64, mSet);
+                ALOGV("Supported feature set : %x", mSet);
 
                 break;
             }
@@ -304,7 +304,7 @@ int WifihalGeneric::handleResponse(WifiEvent &reply)
                         __func__);
                     for(i = 0; i < *mSetSizePtr; i++)
                     {
-                        ALOGV("%" PRIx64, *(mConcurrencySet + i));
+                        ALOGV("%x", *(mConcurrencySet + i));
                     }
                 }
             }
