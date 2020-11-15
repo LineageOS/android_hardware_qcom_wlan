8198b2ccb28b9b6124cce4eddb931d49462ec850
Michael Bestas <mkbestas@lineageos.org>
2020-11-15 02:33:12 +0200

    Merge tag 'LA.UM.9.12.r1-09000-SMxx50.0' of https://source.codeaurora.org/quic/la/platform/hardware/qcom/wlan into lineage-18.0-caf
    "LA.UM.9.12.r1-09000-SMxx50.0"
    
    Conflicts:
            cld80211-lib/Android.mk
            qcwcn/wifi_hal/Android.mk
    
    Change-Id: I62faeba643e06a0392ba43cf33b79fd668dbb8e4

--- qcwcn/wifi_hal/Android.mk
+++ qcwcn/wifi_hal/Android.mk
@@ -96,7 +96,7 @@ LOCAL_SHARED_LIBRARIES += libnl_2
 LOCAL_C_INCLUDES += external/libnl-headers
 endif
 
-LOCAL_HEADER_LIBRARIES := libcutils_headers libutils_headers libwifi-hal-ctrl_headers
+LOCAL_HEADER_LIBRARIES := libcutils_headers libutils_headers libwifi-hal-ctrl_headers libcld80211_headers
 LOCAL_HEADER_LIBRARIES += generated_kernel_headers
 LOCAL_SANITIZE := cfi signed-integer-overflow unsigned-integer-overflow
 
@@ -167,7 +167,7 @@ LOCAL_SHARED_LIBRARIES += libnl_2
 LOCAL_C_INCLUDES += external/libnl-headers
 endif
 
-LOCAL_HEADER_LIBRARIES := libcutils_headers libutils_headers libwifi-hal-ctrl_headers
+LOCAL_HEADER_LIBRARIES := libcutils_headers libutils_headers libwifi-hal-ctrl_headers libcld80211_headers
 LOCAL_HEADER_LIBRARIES += generated_kernel_headers
 LOCAL_SANITIZE := cfi integer_overflow
 include $(BUILD_SHARED_LIBRARY)
