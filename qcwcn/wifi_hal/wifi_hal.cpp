LineageOS/android_hardware_qcom_wlan

commit 8996467be9cb5d5ce95dffb156b07fa62fe496cb

wifihal: Update driver control param to check driver is ready
Write driver control param and wait for successful update to make sure
driver is ready in LONU model.

Bug: 157090295
Test: Basic function test, SSR test
Change-Id: Ia1a690305266baafabe4c2557723f464ca858db1

qcwcn/wifi_hal/wifi_hal.cpp
@@ -919,12 +919,55 @@ unload:
     return ret;
 }
 
+#ifdef WIFI_DRIVER_STATE_CTRL_PARAM
+static int wifi_update_driver_state(const char *state) {
+    struct timespec ts;
+    int len, fd, ret = 0, count = 5;
+    ts.tv_sec = 0;
+    ts.tv_nsec = 200 * 1000000L;
+
+    do {
+        if (access(WIFI_DRIVER_STATE_CTRL_PARAM, R_OK|W_OK) == 0)
+            break;
+        nanosleep(&ts, (struct timespec *)NULL);
+    } while (--count > 0); /* wait at most 1 second for completion. */
+    if (count == 0) {
+        ALOGE("Failed to access driver state control param %s, %d at %s",
+              strerror(errno), errno, WIFI_DRIVER_STATE_CTRL_PARAM);
+        return -1;
+    }
+
+    fd = TEMP_FAILURE_RETRY(open(WIFI_DRIVER_STATE_CTRL_PARAM, O_WRONLY));
+    if (fd < 0) {
+        ALOGE("Failed to open driver state control param at %s",
+              WIFI_DRIVER_STATE_CTRL_PARAM);
+        return -1;
+    }
+
+    len = strlen(state) + 1;
+    if (TEMP_FAILURE_RETRY(write(fd, state, len)) != len) {
+        ALOGE("Failed to write driver state control param at %s",
+              WIFI_DRIVER_STATE_CTRL_PARAM);
+        ret = -1;
+    }
+
+    close(fd);
+    return ret;
+}
+#endif
+
 wifi_error wifi_wait_for_driver_ready(void)
 {
     // This function will wait to make sure basic client netdev is created
     // Function times out after 10 seconds
     int count = (POLL_DRIVER_MAX_TIME_MS * 1000) / POLL_DRIVER_DURATION_US;
     FILE *fd;
+
+#if defined(WIFI_DRIVER_STATE_CTRL_PARAM) && defined(WIFI_DRIVER_STATE_ON)
+    if (wifi_update_driver_state(WIFI_DRIVER_STATE_ON) < 0) {
+        return WIFI_ERROR_UNKNOWN;
+    }
+#endif
 
     do {
         if ((fd = fopen("/sys/class/net/wlan0", "r")) != NULL) {

qcwcn/wifi_hal/Android.mk
@@ -50,6 +50,13 @@ LOCAL_CFLAGS += -Wall -Werror
 # Allow implicit fallthrough in nan_ind.cpp:834 until it is fixed.
 LOCAL_CFLAGS += -Wno-implicit-fallthrough
 
+ifdef WIFI_DRIVER_STATE_CTRL_PARAM
+LOCAL_CFLAGS += -DWIFI_DRIVER_STATE_CTRL_PARAM=\"$(WIFI_DRIVER_STATE_CTRL_PARAM)\"
+ifdef WIFI_DRIVER_STATE_ON
+LOCAL_CFLAGS += -DWIFI_DRIVER_STATE_ON=\"$(WIFI_DRIVER_STATE_ON)\"
+endif
+endif
+
 LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH) \
 	external/libnl/include \
@@ -113,6 +120,13 @@ LOCAL_CLANG_CFLAGS := -Wno-pointer-bool-conversion
 
 # Allow implicit fallthrough in nan_ind.cpp:834 until it is fixed.
 LOCAL_CFLAGS += -Wno-implicit-fallthrough
+
+ifdef WIFI_DRIVER_STATE_CTRL_PARAM
+LOCAL_CFLAGS += -DWIFI_DRIVER_STATE_CTRL_PARAM=\"$(WIFI_DRIVER_STATE_CTRL_PARAM)\"
+ifdef WIFI_DRIVER_STATE_ON
+LOCAL_CFLAGS += -DWIFI_DRIVER_STATE_ON=\"$(WIFI_DRIVER_STATE_ON)\"
+endif
+endif
 
 LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH) \

commit c889ca2c5503fb955f276c4899924c324effd8cc

wifi_hal: Fix access check of control param file
Currently, the driver control param file is created write-only,
so fix the check:

https://android.googlesource.com/kernel/msm-modules/qcacld/+/refs/heads/android-msm-coral-4.14-android11-qpr1/core/hdd/src/wlan_hdd_main.c#190

More importantly the open below uses O_WRONLY as only a write is
necessary, there is no need to check for read access.
Otherwise, WiFi fails to be enabled:

WifiHAL : Failed to access driver state control param Permission denied, 13 at /sys/kernel/boot_wlan/boot_wlan

Fixes: 8996467be9 ("wifihal: Update driver control param to check driver is ready")
Test: Manual build and functionality test
Signed-off-by: Pablo Mendez Hernandez <pablomh@gmail.com>
Signed-off-by: Marijn Suijten <marijn.suijten@somainline.org>
Change-Id: If6308cd790c69d2d57815ef6815c779258005eff

qcwcn/wifi_hal/wifi_hal.cpp
@@ -927,7 +927,7 @@ static int wifi_update_driver_state(const char *state) {
     ts.tv_nsec = 200 * 1000000L;
 
     do {
-        if (access(WIFI_DRIVER_STATE_CTRL_PARAM, R_OK|W_OK) == 0)
+        if (access(WIFI_DRIVER_STATE_CTRL_PARAM, W_OK) == 0)
             break;
         nanosleep(&ts, (struct timespec *)NULL);
     } while (--count > 0); /* wait at most 1 second for completion. */