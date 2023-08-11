e5a1745e0a6f86be03ea0e3d3c0a17bd59c90484
Ricky Cheung <rcheung844@gmail.com>
2023-08-11 11:26:28 +0800

    wcnss-service: Restore OSS building functionality
    * Use OSS libwcnss_qmi whenever TARGET_PROVIDES_WCNSS_QMI is true
    
    Change-Id: Ifa62c8fd448dd4ccac8776a262fc72ae7a3a86c8

--- wcnss-service/Android.mk
+++ wcnss-service/Android.mk
@@ -9,6 +9,10 @@ LOCAL_HEADER_LIBRARIES += vendor_common_inc
 LOCAL_SRC_FILES := wcnss_service.c
 LOCAL_SHARED_LIBRARIES := libc libcutils libutils liblog
 ifeq ($(strip $(TARGET_USES_QCOM_WCNSS_QMI)),true)
+ifeq ($(TARGET_PROVIDES_WCNSS_QMI),true)
+LOCAL_CFLAGS += -DWCNSS_QMI_OSS
+LOCAL_SHARED_LIBRARIES += libdl
+else
 LOCAL_CFLAGS += -DWCNSS_QMI -DMDM_DETECT
 LOCAL_HEADER_LIBRARIES += libqmi_common_headers
 LOCAL_SRC_FILES += wcnss_qmi_client.c device_management_service_v01.c
@@ -16,6 +20,7 @@ LOCAL_SHARED_LIBRARIES += libqmi_cci libqmi_common_so
 LOCAL_HEADER_LIBRARIES += libmdmdetect_headers
 LOCAL_SHARED_LIBRARIES += libmdmdetect
 LOCAL_HEADER_LIBRARIES += libril-qc-qmi-services-headers
+endif #TARGET_PROVIDES_WCNSS_QMI
 endif #TARGET_USES_QCOM_WCNSS_QMI
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -Wall -Werror
