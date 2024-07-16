0c1caf71528ac1ce4960eb6a68aeb0ff62f7928b
Ricky Cheung <rcheung844@gmail.com>
2024-07-16 18:58:14 +0800

    wcnss-service: Allow using WCNSS QMI without building with BSP
    Currently, wcnss-service will generate a default MAC address set in
    WCNSS_qcom_cfg.ini for devcies building without a custom libwcnss_qmi.
    This is not ideal and does not meet charter requirements for LineageOS.
    
    This patch introductes a new part of the service that loads QMI/QCCI
    functions via libdl, without requiring the use of most headers from
    QCOM BSPs (so that we won't receive a DMCA complaint from QCOM),
    while utilizing the existing open-sourced components.
    
    In missing_qmi_client_api.h, missing magic constants, functions and
    types are pulled from generic device blobs. Only required structs
    are created for the purposes of this library. These include:
    * dms_get_mac_address_req_msg_v01
    * dms_get_mac_address_resp_msg_v01
    
    To enable this feature for a device, the following needs to be added
    to BoardConfig.mk:
    TARGET_USES_QCOM_WCNSS_QMI := true
    TARGET_WCNSS_QMI_INCLUDE_DIR := $(DEVICE_PATH)/wifi/include
    
    A custom header file named missing_constants.h has to be created
    under the device tree. The directory containing this header file
    needs to be defined under TARGET_WCNSS_QMI_INCLUDE_DIR.
    Otherwise, you should not define TARGET_USES_QCOM_WCNSS_QMI to true.
    
    The header file should consist of a few constants required by
    libqmiservices.so. These constants are compiled into the library and
    is originally located in the BSPs. However, they can be obtained
    easily with a decompiler.
    
    Please note that for each QMI stack upgrade, it is possible that the
    constants would be modified. In that case, the maintainer would
    have to update these values manually.
    
    A sample of the header library can be found at the following link:
    https://gist.github.com/TipzRickyCheung/c4a15690a4ccf713114944db0db1d98b
    
    For testing, dms_get_service_object_shimshim_v01() is implemented to
    test for DMS_V01_IDL_MINOR_VERS, the only value observed to change
    between different QMI blobs.
    
    Before shipping a device with this feature, please note that some
    of them will obtain the MAC address in reverse of what is expected.
    It is best advised to check the generated MAC address supplied in
    your logs against a list of vendor OUIs to make sure it is what would
    be expected. Set TARGET_USES_WCNSS_MAC_ADDR_REV to true if it is
    reversed.
    
    This patch should only support legacy-um and newer devices.
    Older ones from before legacy-um (e.g. MSM8916, MSM8994) may require
    some changes to wcnss_qmi_client.c (or old blobs in general).
    Since LineageOS dropped support for them in Android 12, no impact
    will be observed.
    
    Devices that does not use this feature is unaffected by this change.
    Maintainers may continue to use prebuilt wcnss_service blobs if using
    this feature is found to be unsatisfactory.
    
    Co-authored-by: Quallenauge <Hamsi2k@freenet.de>
    Test: boots, verified device MAC address is correct
    Change-Id: Ie77c56b6fae55ea451f1cacad4015d290c81e7c5

--- wcnss-service/Android.mk
+++ wcnss-service/Android.mk
@@ -12,10 +12,7 @@ ifeq ($(strip $(TARGET_USES_QCOM_WCNSS_QMI)),true)
 ifeq ($(TARGET_USES_WCNSS_MAC_ADDR_REV),true)
 LOCAL_CFLAGS += -DWCNSS_QMI_MAC_ADDR_REV
 endif
-ifeq ($(TARGET_PROVIDES_WCNSS_QMI),true)
-LOCAL_CFLAGS += -DWCNSS_QMI_OSS
-LOCAL_SHARED_LIBRARIES += libdl
-else
+ifneq ($(QCPATH),)
 LOCAL_CFLAGS += -DWCNSS_QMI -DMDM_DETECT
 LOCAL_HEADER_LIBRARIES += libqmi_common_headers
 LOCAL_SRC_FILES += wcnss_qmi_client.c device_management_service_v01.c
@@ -23,7 +20,21 @@ LOCAL_SHARED_LIBRARIES += libqmi_cci libqmi_common_so
 LOCAL_HEADER_LIBRARIES += libmdmdetect_headers
 LOCAL_SHARED_LIBRARIES += libmdmdetect
 LOCAL_HEADER_LIBRARIES += libril-qc-qmi-services-headers
+else
+LOCAL_CFLAGS += -DWCNSS_QMI
+LOCAL_SHARED_LIBRARIES += libdl
+ifeq ($(TARGET_PROVIDES_WCNSS_QMI),true)
+LOCAL_CFLAGS += -DWCNSS_QMI_OSS
+else
+LOCAL_SRC_FILES += wcnss_qmi_client.c missing_qmi_client_api.c
+LOCAL_CFLAGS += -DWCNSS_QMI_PRIVATE
 endif #TARGET_PROVIDES_WCNSS_QMI
+ifneq ($(TARGET_WCNSS_QMI_INCLUDE_DIR),)
+LOCAL_C_INCLUDES += $(TARGET_WCNSS_QMI_INCLUDE_DIR)
+else
+LOCAL_CFLAGS += -DENABLE_DMS_SHIM
+endif #TARGET_WCNSS_QMI_INCLUDE_DIR
+endif #QCPATH
 endif #TARGET_USES_QCOM_WCNSS_QMI
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -Wall -Werror
