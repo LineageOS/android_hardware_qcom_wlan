2fbe772a31cef7b9117127094c2158efa5d2330c
Vinay Gannevaram <quic_vganneva@quicinc.com>
2024-08-12 11:27:00 +0530

    Handle NAN pairing when user rejects the connection
    User can accept or reject the pairing connection request received from
    the pairing initiator. When user rejects the connection, ignore the PASN
    Auth 1 frame received and send PASN Auth 2 frame with status as failure
    to the initiator.
    
    CRs-Fixed: 3895976
    Change-Id: I303115370a3cf051329be5bb23fefb89c162be06

--- qcwcn/wifi_hal/nan_pairing.cpp
+++ qcwcn/wifi_hal/nan_pairing.cpp
@@ -723,6 +723,13 @@ void nan_rx_mgmt_auth(wifi_handle handle, const u8 *frame, size_t len)
     struct wpa_pasn_params_data pasn_data;
     struct nan_pairing_peer_info *peer;
     struct ieee80211_mgmt *mgmt = (struct ieee80211_mgmt *) frame;
+    NanCommand *nanCommand = NULL;
+
+    nanCommand = NanCommand::instance(handle);
+    if (nanCommand == NULL) {
+        ALOGE("%s: Error NanCommand NULL", __FUNCTION__);
+        return;
+    }
 
     if (!info || !info->secure_nan) {
         ALOGE("%s: secure nan NULL", __FUNCTION__);
@@ -772,7 +779,18 @@ void nan_rx_mgmt_auth(wifi_handle handle, const u8 *frame, size_t len)
                             peer->bssid, pasn_get_cipher(pasn), nanPMKLifetime,
                             pasn_get_ptk(pasn), NULL, NULL, pasn_get_akmp(pasn));
             memset(pasn_get_ptk(pasn), 0, sizeof(struct wpa_ptk));
-        } else if (ret == -1) {
+        } else if (ret == -1 || mgmt->u.auth.status_code) {
+            NanPairingConfirmInd evt;
+
+            memset(&evt, 0, sizeof(NanPairingConfirmInd));
+            evt.pairing_instance_id = peer->pairing_instance_id;
+            evt.rsp_code = NAN_PAIRING_REQUEST_REJECT;
+            evt.reason_code = NAN_STATUS_PROTOCOL_FAILURE;
+            evt.enable_pairing_cache = 0;
+            nanCommand->handleNanPairingConfirm(&evt);
+
+            peer->is_paired = false;
+            peer->is_pairing_in_progress = false;
             wpa_pasn_reset(pasn);
             ALOGE(" %s wpa_pasn_auth_rx failed", __FUNCTION__);
             peer->peer_role = SECURE_NAN_IDLE;
