0471d488f2e9f6a943c65c87a10480966efd716e
Vinay Gannevaram <quic_vganneva@quicinc.com>
2024-08-27 12:43:43 +0530

    wifihal: Disable support for Groupkeys
    When pairing support is enabled, wifihal is enabling support for
    groupkeys by default. However since groupkeys are not allowed by
    the underlying drivers for some targets, disable support for groupkeys.
    
    CRs-Fixed: 3906217
    Change-Id: I9f25ef07d0ea40908df898183f88e2d6127f6e7e

--- qcwcn/wifi_hal/nan.cpp
+++ qcwcn/wifi_hal/nan.cpp
@@ -266,8 +266,6 @@ wifi_error nan_publish_request(transaction_id id,
         info->secure_nan->supported_bootstrap =
               msg->nan_pairing_config.supported_bootstrapping_methods;
 #ifdef WPA_PASN_LIB
-        if (!msg->cipher_capabilities && msg->nan_pairing_config.enable_pairing_setup)
-            msg->cipher_capabilities = 0x4;
         if (!info->secure_nan->dev_grp_keys)
             nan_pairing_derive_grp_keys(info, t_nanCommand->getNmi(),
                                         msg->cipher_capabilities);
@@ -402,8 +400,6 @@ wifi_error nan_subscribe_request(transaction_id id,
         info->secure_nan->supported_bootstrap =
               msg->nan_pairing_config.supported_bootstrapping_methods;
 #ifdef WPA_PASN_LIB
-        if (!msg->cipher_capabilities && msg->nan_pairing_config.enable_pairing_setup)
-            msg->cipher_capabilities = 0x4;
         if (!info->secure_nan->dev_grp_keys)
             nan_pairing_derive_grp_keys(info, t_nanCommand->getNmi(),
                                         msg->cipher_capabilities);
