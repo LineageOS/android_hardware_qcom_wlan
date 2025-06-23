cfd8205057cf7a6ba7e86b4db779c5e91bbd7610
Kashish Awasthi <kawasthi@qti.qualcomm.com>
2025-06-23 12:37:29 +0530

    wifi-hal: Fix uninitialized variable and TWT attr error handling
    Initialized the variable 'exp' to 1 to prevent potential use of
    an uninitialized value and added error handling for
    TWT_SETUP_WAKE_INTVL_EXP.
    
    CRs-Fixed: 4188627
    Change-Id: I9a22e80dd97ce32fcc44e50b3e864b356cb0ea70

--- qcwcn/wifi_hal/twt.cpp
+++ qcwcn/wifi_hal/twt.cpp
@@ -909,7 +909,7 @@ int TwtCommand::handleEvent(WifiEvent &event)
         {
             wifi_twt_session twt_session;
             wifi_twt_error_code error_code;
-            u32 exp, mantissa;
+            u32 exp = 1, mantissa;
 
             if (!tb_vendor[QCA_WLAN_VENDOR_ATTR_CONFIG_TWT_PARAMS]) {
                 ALOGE("TWT session setup nested attributes is null");
@@ -962,8 +962,15 @@ int TwtCommand::handleEvent(WifiEvent &event)
                 ALOGE("TWT_SETUP_WAKE_DURATION is missing");
 
             attr_id = QCA_WLAN_VENDOR_ATTR_TWT_SETUP_WAKE_INTVL_EXP;
-            if (tb2[attr_id])
-                exp = pow(2, get_u8(tb2[attr_id]));
+            if (!tb2[attr_id]) {
+                ALOGE("TWT_SETUP_WAKE_INTVL_EXP attribute is missing");
+                if (mHandler.on_twt_failure)
+                    (*mHandler.on_twt_failure)(mRequestId, WIFI_TWT_ERROR_CODE_INVALID_PARAMS);
+                else
+                    ALOGE("TWT: Failure Callback is not registered");
+                return NL_SKIP;
+            }
+            exp = pow(2, get_u8(tb2[attr_id]));
 
             attr_id = QCA_WLAN_VENDOR_ATTR_TWT_SETUP_WAKE_INTVL2_MANTISSA;
             if (!tb2[attr_id]) {
